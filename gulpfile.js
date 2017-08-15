b0d03eaa83bf5047738bfd0c020bc5a6aac92403
Michael <mveronie@gmail.com>
2017-08-15 02:54:57 -0400

    Added postcss plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', () => {
   console.log("Hooray, you created a gulp task.");
@@ -10,7 +14,9 @@ gulp.task('html', () => {
 });
 
 gulp.task('styles', () => {
-  console.log("Imagine Sass or PostCss tasks running here.");
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', () => {
