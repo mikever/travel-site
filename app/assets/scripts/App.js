634dd8e99e5c657625c1b2a808bfad470fe03c10
Michael <mveronie@gmail.com>
2017-09-26 06:04:48 -0400

    Added javascript functionality to mobile menu bar.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,3 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-  paytaxes() {
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
-
-alert("ABC 321");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.paytaxes();
-
-$("h1").remove();
+let mobileMenu = new MobileMenu();
